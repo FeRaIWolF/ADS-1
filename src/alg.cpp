19d35d6c123cb5e8a8a8ad688a511c841549dc87
FeRaIWolF <79052930+FeRaIWolF@users.noreply.github.com>
2021-03-17 13:10:44 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,21 @@
+// Copyright 2021 NNTU-CS
+int cbinsearch(int* arr, int size, int value) {
+int i = 0, j = size - 1, count = 0;
+while (i < j) {
+int mid = i + (j - 1) / 2;
+if (arr[mid] > value)
+j = mid;
+else
+i = mid + 1;
+}
+if (arr[i] == value) {
+while (arr[i] == value) {
+count++;
+i++;
+}
+}
+if (count)
+return count;
+else
+return 0;
+}
